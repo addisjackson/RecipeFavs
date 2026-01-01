2bd800aab0323a995b56ee4586b4ee9bb8b89078
Addis Jackson <addisjackson@pursuit.org>
2026-01-01 15:22:18 -0500

    fixes

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RecipeFavs",
-  server: {
-  host: true,
-  strictPort: true,
-  hmr: {
-    protocol: "ws",
-    host: "localhost",
-    port: 5173
-  },
-  build: { 
-    outDir: "dist", 
-    sourcemap: false, 
-    minify: "esbuild", 
-    cssMinify: true, 
-  }
-}
+  base: "/RecipeFavs/",
 
 })
